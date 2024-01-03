154a1cf7d8f3b42d3993d77b7265fa57cdfc12ea
Caden Wilcox <wilcox4357@gmail.com>
2024-01-02 23:05:53 -0800

    font decided home page v1 done

--- quizl/src/App.jsx
+++ quizl/src/App.jsx
@@ -2,6 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import axios from 'axios'
 import Question from './components/Question'
+import { motion } from 'framer-motion'
+// ! add a feature to get a hint for the question
+// ! Timed rounds
+// ! Add a leaderboard
+// ! Add a feature to choose a category
+// ! Add a feature to choose the number of questions
+// ! Add a feature to choose the difficulty of the questions
+// ! organize components better to resemble a real app
 function App() {
   const [score, setScore] = useState(0)
   const [questions, setQuestions] = useState([])
@@ -9,24 +17,33 @@ function App() {
   const [hideButton, setHideButton] = useState(false)
   const beginQuiz = async (e) => {
     try{
-      const response = await axios.get('https://the-trivia-api.com/v2/questions')
+      const response = await axios.get('https://the-trivia-api.com/v2/questions?categories=music')
       response.data.forEach((question) => {
         question.incorrectAnswers.push(question.correctAnswer)
         question.incorrectAnswers.sort(() => Math.random() - 0.5)
       })
       setQuestions(response.data)
-      e.target.classList.toggle('hidden')
+      setTimeout(() => {
+        e.target.classList.toggle('hidden')
+      }, 200);
     }
     catch(error){
       console.log(error)
     }
   }
   return (
-    <>
-      <h1 className='text-6xl underline mb-4 text-purple-600'>Quizl</h1>
-      <h1 className='text-4xl underline mb-4 text-purple-600'>Random Trivia Game</h1>
+    <div>
+      <h1 className='text-8xl underline mb-4 text-purple-600'>QuizL</h1>
+      <h1 className='text-4xl underline mb-4 text-purple-600'>All In One Trivia Game</h1>
 
-      <button className='border p-2 bg-purple-400 rounded-xl font-bold' onClick={beginQuiz} >Click to begin!</button>
+      <motion.div 
+      className='text-black text-3xl p-2 bg-purple-400 rounded-xl w-fit mx-auto cursor-pointer' 
+      onClick={beginQuiz} 
+      initial={{ scale: .75 }}
+      animate={{ scale: 1.25 }}
+      transition={{ duration: 1, repeat: Infinity, repeatType: 'reverse', ease: 'easeInOut'}}
+    
+      >Click to begin!</motion.div>
       <div>
         {
           questions.length > 0 && (
@@ -34,7 +51,7 @@ function App() {
           )
         }
       </div>
-    </>
+    </div>
   )
 }
 
--- quizl/src/components/Question.jsx
+++ quizl/src/components/Question.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
-
+import React, { useRef, useState } from 'react';
+import { motion } from "framer-motion"
 const Question = ({ question, currentQuestion, setCurrentQuestion, score, setScore }) => {
 
     const [selectedAnswer, setSelectedAnswer] = useState('')
     const [checked, setChecked] = useState(false)
     const [submitted, setSubmitted] = useState(false)
+
     const nextQuestion = () => {
         if (selectedAnswer === question.correctAnswer) {
             setScore(score + 1)
@@ -22,30 +23,36 @@ const Question = ({ question, currentQuestion, setCurrentQuestion, score, setSco
         setSubmitted(true)
     }
     return (
-        <div>
-            <div>
-                <h3>{question.question.text}</h3>
+        <motion.div
+            className='border p-10 bg-slate-400'
+            initial={{  y: -1000 }}
+            animate={{  y: 0 }}
+            transition={{ duration: 5, type: 'spring', bounce: .25, stiffness: 100 }}
+        >
+            
+            <div className='border border-red-500 w-2/4 mx-auto p-16 bg-indigo-900'>
+                <h3 className='text-2xl'>{question.question.text}</h3>
                 <ul>
                     {question.incorrectAnswers.map((answer, idx) => (
                         <div key={idx} style={{ width: '300px', textAlign: 'left', margin: '0 auto' }}>
                             <input name='answer' type='radio' onClick={() => handleChange(answer)} />
-                            <label>{answer}</label>
+                            <label className='text-xl ml-2' >{answer}</label>
                         </div>
                     ))}
                 </ul>
                 {
-                    currentQuestion === 9 ? <button className='border p-2 bg-purple-400 rounded-xl font-bold' onClick={submitQuiz}>Submit</button> : <button className='border p-2 bg-purple-400 rounded-xl font-bold' onClick={nextQuestion}>Next {currentQuestion + 1}/10</button>
+                    currentQuestion === 9 ? <button className='m-4 text-lg border p-2 bg-purple-400 rounded-xl font-bold text-black' onClick={submitQuiz}>Submit</button> : <button className='m-4 text-lg border p-2 bg-purple-400 rounded-xl font-bold text-black' onClick={nextQuestion}>Next {currentQuestion + 1}/10</button>
                 }
                 {
                     submitted &&
                     <div>
                         <h2>Final Score: {score}/10</h2>
-                        <button className='border p-2 bg-purple-400 rounded-xl font-bold' onClick={() => window.location.reload()}>Restart</button>    
+                        <button className='m-4 text-lg border p-2 bg-purple-400 rounded-xl font-bold text-black' onClick={() => window.location.reload()}>Restart</button>    
                     </div>
 
                 }
             </div>
-        </div>
+        </motion.div>
     )
 }
 
